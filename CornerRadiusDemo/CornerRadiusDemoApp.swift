f575def8d2bafffa7711c3a806d69f9aad56385a
Davide Troise <davide@troise.net>
2023-01-31 22:17:35 +0100

    Initial Commit

---
+++ CornerRadiusDemo/CornerRadiusDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  CornerRadiusDemoApp.swift
+//  CornerRadiusDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct CornerRadiusDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
