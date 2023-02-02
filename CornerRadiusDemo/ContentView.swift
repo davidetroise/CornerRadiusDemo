ffb165c9540a26e102161c21386b688cc989dfde
Davide Troise <davide@troise.net>
2023-02-02 16:51:05 +0100

    Complete code

--- CornerRadiusDemo/ContentView.swift
+++ CornerRadiusDemo/ContentView.swift
@@ -9,12 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let maxRadius: CGFloat = 50.0
+    
+    @State private var topLeft: CGFloat = 0.0
+    @State private var topRight: CGFloat = 0.0
+    @State private var bottomLeft: CGFloat = 0.0
+    @State private var bottomRight: CGFloat = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("\(Int(topLeft))")
+                Slider(value: $topLeft, in: 0...maxRadius)
+                
+                Spacer()
+                Slider(value: $topRight, in: 0...maxRadius)
+                Text("\(Int(topRight))")
+            }
+            
+            Button {
+                //
+            } label: {
+                Text("Rounded button")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .frame(width: max(200, maxRadius * 2), height: maxRadius * 2)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(topLeft, corner: .topLeft)
+                    .cornerRadius(topRight, corner: .topRight)
+                    .cornerRadius(bottomLeft, corner: .bottomLeft)
+                    .cornerRadius(bottomRight, corner: .bottomRight)
+            }
+
+            
+            HStack {
+                Text("\(Int(bottomLeft))")
+                Slider(value: $bottomLeft, in: 0...maxRadius)
+                
+                Spacer()
+                Slider(value: $bottomRight, in: 0...maxRadius)
+                Text("\(Int(bottomRight))")
+            }
         }
         .padding()
     }
---
+++ CornerRadiusDemo/RoundedCorner.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedCorner.swift
+//  CornerRadiusDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat
+    var corner: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corner, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corner: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corner: corner))
+    }
+}
